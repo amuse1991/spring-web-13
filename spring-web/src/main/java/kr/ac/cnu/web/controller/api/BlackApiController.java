4d57b8d3144173719ebc5048b1ebacdc2ab37712
YoonHo Shin <amuse1991@gmail.com>
2018-06-06 15:59:10 +0900

    some change in issue #24

--- spring-web/src/main/java/kr/ac/cnu/web/controller/api/BlackApiController.java
+++ spring-web/src/main/java/kr/ac/cnu/web/controller/api/BlackApiController.java
@@ -48,7 +48,7 @@ public class BlackApiController {
         }
 
         // TODO new user
-        User user = new User(name, 50000);
+        User user = new User(name, 1900);
 
         // TODO save in repository
         return userRepository.save(user);
